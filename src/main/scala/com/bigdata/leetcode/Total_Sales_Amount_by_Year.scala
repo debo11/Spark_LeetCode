a4ed493d8f5dce31d08c677cc7d41442d2376c15
Debashish <chaudharydebashish@gmail.com>
2023-02-16 19:48:33 +0530

    Sales code updated

--- src/main/scala/com/bigdata/leetcode/Total_Sales_Amount_by_Year.scala
+++ src/main/scala/com/bigdata/leetcode/Total_Sales_Amount_by_Year.scala
@@ -1,7 +1,7 @@
 package com.bigdata.leetcode
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, datediff, substring, when, year}
+import org.apache.spark.sql.functions.{col, concat, datediff, lit, substring, when, year}
 
 object Total_Sales_Amount_by_Year {
   def main(args: Array[String]) {
@@ -27,6 +27,19 @@ object Total_Sales_Amount_by_Year {
     val oneyeardiff_df = df_yeardiff.selectExpr("prod_id","end_year")
     val twoyeardiff_df = df_yeardiff.selectExpr("prod_id","middle_year")
 
-    val uniondistinct_df = sameyear_df.union(oneyeardiff_df).union(twoyeardiff_df).distinct().filter(col("start_year") =!= "null").orderBy("prod_id").select(col("prod_id"),col("start_year").alias("report_year")).show()
+    val uniondistinct_df = sameyear_df.union(oneyeardiff_df).union(twoyeardiff_df).distinct().filter(col("start_year") =!= "null").orderBy("prod_id").select(col("prod_id"),col("start_year").alias("report_year"))
+    uniondistinct_df.show()
+
+  //merging above df
+    val union_df = uniondistinct_df.join(df_yeardiff, uniondistinct_df("prod_id") === df_yeardiff("prod_id"),"inner").orderBy("product_id")
+    union_df.show()
+
+    //logic for total days per year
+    val daysperyear_df = union_df.withColumn("year_days",when(col("yearDifference") === "0", col("total_days")).when(col("yearDifference") === "1" && col("report_year") === col("start_year"), datediff(concat(col("start_year"),lit("-12-31")) ,col("start_date"))+1).when(col("yearDifference") === "1" && col("report_year") === col("end_year"), datediff(col("end_date"), concat(col("end_year"),lit("-01-01")) )+1).when(col("yearDifference") === "2" && col("report_year") === col("start_year"), datediff(concat(col("start_year"),lit("-12-31")) ,col("start_date"))+1).when(col("yearDifference") === "2" && col("report_year") === col("middle_year"), 365).when(col("yearDifference") === "2" && col("report_year") === col("end_year"), datediff(col("end_date"), concat(col("end_year"),lit("-01-01")) )+1).otherwise("null")).selectExpr("product_id","product_name","report_year","average_daily_sales","year_days")
+    daysperyear_df.show()
+
+    //sales per year calculation
+    val salescalc_df = daysperyear_df.withColumn("total_amount", col("average_daily_sales") * col("year_days")).selectExpr("product_id","product_name","report_year","total_amount")
+    salescalc_df.show()
   }
 }
