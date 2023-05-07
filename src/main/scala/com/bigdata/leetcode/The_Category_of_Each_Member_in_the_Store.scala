03f21e92d3af4a5d253ae6ec301e7ea5740b5710
Debashish <chaudharydebashish@gmail.com>
2023-05-07 10:24:47 +0530

    The_Category_of_Each_Member_in_the_Store

--- src/main/scala/com/bigdata/leetcode/The_Category_of_Each_Member_in_the_Store.scala
+++ src/main/scala/com/bigdata/leetcode/The_Category_of_Each_Member_in_the_Store.scala
@@ -4,7 +4,7 @@ import org.apache.ivy.util.Message.info
 import org.apache.spark.sql.functions.{col, lit, when}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-class he_Category_of_Each_Member_in_the_Store {
+class The_Category_of_Each_Member_in_the_Store {
   val spark = SparkSession.builder.master("local").appName("Find the Start and End Number of Continuous Ranges").getOrCreate()
 
   //reading inputs
@@ -25,12 +25,12 @@ class he_Category_of_Each_Member_in_the_Store {
     info("merging purchase with visit to find out customer who purchased")
     val purchasevisit_df = purchases_input_df.join(visits_input_df, purchases_input_df("visit_id") === visits_input_df("visit_id"), "left").select("member_id").groupBy("member_id").count().select(col("member_id"), col("count").alias("purchase_count"))
 
-   info("merging customervisit and finalcustomervisit for resultant calculation")
+    info("merging customervisit and finalcustomervisit for resultant calculation")
     val finalpurchasevisit_df = finalcutomervist_df.join(members_input_df, finalcutomervist_df("cust_name") === members_input_df("name"), "left").select("cust_name","visit_count","member_id").withColumnRenamed("member_id","mem_id")
     val finalmerge_df = finalpurchasevisit_df.join(purchasevisit_df, finalpurchasevisit_df("mem_id") === purchasevisit_df("member_id"), "left").select("cust_name","mem_id","visit_count","purchase_count")
     val categorycalc_df = finalmerge_df.withColumn("category_value", (lit(100) * col("purchase_count")) / (col("visit_count")))
     val categorydistribution_df = categorycalc_df.withColumn("category", when(col("visit_count") === 0 && col("purchase_count").isNull, lit("Bronze")).when(col("visit_count").isNotNull && col("purchase_count").isNull, lit("Silver")).when(col("category_value") >= 80, lit("Diamond")).when(col("category_value").between(50,80), lit("Gold")).when(col("category_value") < 50, lit("Silver")).otherwise("NA")).select("mem_id","cust_name","category").orderBy("mem_id")
-      categorydistribution_df
+    categorydistribution_df
 
   }
 
@@ -43,11 +43,11 @@ class he_Category_of_Each_Member_in_the_Store {
 }
 
 
-object he_Category_of_Each_Member_in_the_Store {
+object The_Category_of_Each_Member_in_the_Store {
   def main(args: Array[String]) {
-    val he_Category_of_Each_Member_in_the_Store = new he_Category_of_Each_Member_in_the_Store()
+    val The_Category_of_Each_Member_in_the_Store = new The_Category_of_Each_Member_in_the_Store()
 
-    def apply(): he_Category_of_Each_Member_in_the_Store = he_Category_of_Each_Member_in_the_Store
+    def apply(): The_Category_of_Each_Member_in_the_Store = The_Category_of_Each_Member_in_the_Store
 
   }
 }
